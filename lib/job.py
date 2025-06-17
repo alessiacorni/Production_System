83d82295f5ca11fa74d6548f9417aadaf0befab6
alessiacorni <cornialessia@gmail.com>
2025-06-17 18:45:15 +0200

    Welch method to find out the steady state is added

--- lib/job.py
+++ lib/job.py
@@ -19,6 +19,7 @@ class Job:
         routing: list['Server'],
         arrival_time: float,
         process_time: float,
+        due_date: float,
         idx: int,
         family: str
     ) -> None:
@@ -26,25 +27,35 @@ class Job:
         self.routing = routing
         self.arrival_time = arrival_time
         self.process_time = process_time
+        self.due_date = due_date
         self.idx = idx
         self.delay: float | None = None
         self.done: bool = False
         self.family = family
+        self.completion_time: float | None = None
+        self.time_in_system: float | None = None
 
     def main(self) -> Generator[Request | Process, Any, None]:
+        start_time_in_system = self.env.now
         for server in self.routing:
             with server.request() as request:
                 queue_entry_time = self.env.now
 
-                # print("PRODUCT {} of family {}: Waiting for the machine {} to become available".format(self.idx, self.family, server.name))
+                # if self.family == "F3":
+                  #  print("PRODUCT {} of family {}: Waiting for the machine {} to become available".format(self.idx, self.family, server.name))
                 yield request
-                # print("PRODUCT {} of family {}: Machine {} ready".format(self.idx, self.family, server.name))
+                #if self.family == "F3":
+                 #   print("PRODUCT {} of family {}: Machine {} ready".format(self.idx, self.family, server.name))
 
                 queue_exit_time = self.env.now
                 self.delay = queue_exit_time - queue_entry_time
 
-                # print("PRODUCT {} of family {}: starting to be processed by the machine {}".format(self.idx, self.family, server.name))
+                # if self.family == "F3":
+                  #  print("PRODUCT {} of family {}: starting to be processed by the machine {}".format(self.idx, self.family, server.name))
                 yield self.env.process(server.process_job(self))
-                # print("PRODUCT {} of family {}: Machine {} process finished".format(self.idx, self.family, server.name))
+                # if self.family == "F3":
+                  #  print("PRODUCT {} of family {}: Machine {} process finished".format(self.idx, self.family, server.name))
 
         self.done = True
+        self.completion_time = self.env.now
+        self.time_in_system = self.completion_time - start_time_in_system
